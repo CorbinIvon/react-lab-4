ecb8b13e28c1125ada5b768b490dee54766ebd1a
Corbin Meier <corbinivon@gmail.com>
2023-11-26 15:45:05 -0800

    Adds instruction requirements to Field.jsx

--- src/app/components/Field.jsx
+++ src/app/components/Field.jsx
@@ -1,5 +1,8 @@
 export default function Field({ children }) {
-  // render children inside a div with a flex-col layout 
+  // render children inside a div with a flex-col layout
+  return (
+    <div className="flex flex-col">
+      {children}
+    </div>
+  );
 }
-// Instructions:
-// 
